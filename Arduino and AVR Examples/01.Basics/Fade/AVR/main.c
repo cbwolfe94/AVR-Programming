b33dd990a40d2b0e636f077dc0d9fcb282988248
Conner Wolfe <37260117+cbwolfe94@users.noreply.github.com>
2020-09-09 16:28:03 -0400

    AVR C implementation of Fade

--- Arduino and AVR Examples/01.Basics/Fade/AVR/main.c
+++ Arduino and AVR Examples/01.Basics/Fade/AVR/main.c
@@ -1,22 +1,32 @@
 #ifndef __AVR_ATmega328P__
-    #define __AVR_ATmega328P__
+#define __AVR_ATmega328P__
 #endif
 
 #include <avr/io.h>
+#include <avr/delay.h>
+#include <Timers.h>
+
+
 
 
 int main() {
+    uint8_t fadeAmount = 5;
+    uint8_t fadeDelay = 50;
 
-    DDRB |= (1<<PB1)|(1<<PB2);
-    TCCR1A |= (1<<WGM10);
-    TCCR1B |= (1<<WGM12)|(1<<CS10);
-    TCCR1A |= (1<<COM1A1)|(1<<COM1B1);
-    OCR1A = 127;
-    OCR1B = 63;
+    DDRB |= (1 << PB1);
 
-    while(1) {
+    initFastPWM16Bit(1, 0, 0);
+
+    while (1) {
+        OCR1A = OCR1A + fadeAmount;
+
+        if (OCR1A <= 0 || OCR1A >= 255) {
+            fadeAmount = -fadeAmount;
+            }
+        _delay_ms(fadeDelay);
         
     }
+    
 
     return 0;
 }
